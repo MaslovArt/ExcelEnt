c020684d0fec3a4a93c204f9b6df7b1541927696
Маслов Артем <artemiy_maslov@mail.ru>
2021-04-24 15:15:10 +0300

    Use inherited InnerException prop instead of custom

--- ExcelEnt/Exceptions/ExcelBindException.cs
+++ ExcelEnt/Exceptions/ExcelBindException.cs
@@ -5,14 +5,13 @@ namespace ExcelEnt.Exceptions
     internal class ExcelBindException : Exception
     {
         public ExcelBindException(int row, int col, Exception ex)
+            :base(ex.Message, ex)
         {
             Row = row;
             Col = col;
-            Inner = ex;
         }
 
         public int Row { get; set; }
         public int Col { get; set; }
-        public Exception Inner { get; set; }
     }
 }
